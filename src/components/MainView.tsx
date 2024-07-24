a80b99e9e580d5a11b9a592e4065415860200d02
Ipelcaztreortega <118202882+Ipelcaztreortega@users.noreply.github.com>
2024-07-24 12:37:21 -0400

    Updated some minimal changes

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -5,7 +5,7 @@ export const MainView = () => {
                 <p className="text-base sm:text-lg text-red-900">Hi, my name is</p>
                 <h1 className="mb-2 text-3xl sm:mb-4 sm:text-5xl md:text-6xl lg:text-7xl font-bold">Irvin Pelcaztre-Ortega.</h1>
                 <h1 className="mb-4 text-3xl sm:mb-8 sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-600">I build things for my growth.</h1>
-                <p className="text-base sm:text-lg text-gray-600">I'm an aspiring Software Engineer currently looking for an internship for Summer 2024</p>
+                <p className="text-base sm:text-lg text-gray-600">I'm an aspiring Software Engineer currently looking for a full time role starting this January 2025</p>
             </div>
         </div>
     );
